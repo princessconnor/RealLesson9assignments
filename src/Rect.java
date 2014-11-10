28ec8626dfa7ca095e34bb8f6fba67240635a840
John Martens <John.Martens@gmail.com>
2014-11-10 09:18:28 -0500

    Day 2 of Shape example

--- src/Rect.java
+++ src/Rect.java
@@ -1,6 +1,66 @@
+
+import TurtleGraphics.Pen;
+
 public class Rect implements Shape{
-    public double area() {
-      return 0;
+    
+      private double xPos, yPos, width, height;
+    
+    public Rect(){
+        xPos=0;
+        yPos=0;
+        width=1;
+        height=1;
     }
     
+    public Rect(double x, double y, double w, double h){
+        xPos=x;
+        yPos=y;
+        width=w;
+        height=h;
+    }
+    
+    public double area() {
+      return width * height;
+    }
+
+    @Override
+    public void draw(Pen p) {
+        p.up();
+        p.move(xPos, yPos);
+        p.down();
+        p.setDirection(0);
+        p.move(width);
+        p.turn(90);
+        p.move(height);
+        p.turn(90);
+        p.move(width);
+        p.turn(90);
+        p.move(height);
+      }
+
+    public double getXPos() {
+       return xPos;
+    }
+
+    public double getYPos() {
+        return xPos;
+    }
+
+    public void move(double xLoc, double yLoc) {
+       xPos=xLoc;
+       yPos=yLoc;
+    }
+
+    public void stretchBy(double factor) {
+        height*=factor;
+        width*=factor;
+    }
+    
+    public String toString(){
+        String str="This is a RECTANGLE\n";
+        str += "XLOC: " + xPos + "\tYLOC: " + yPos + "\n";
+        str += "WIDTH: " + width + "\tHEIGHT: " + height + "\n";
+        str += "AREA: " + area();
+        return str;
+    }
 }
