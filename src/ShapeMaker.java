623e82ee7d2b12ff6df6fcc26fc80d217a1aa83e
John Martens <John.Martens@gmail.com>
2014-11-11 08:45:30 -0500

    Finished Shape Part 1

--- src/ShapeMaker.java
+++ src/ShapeMaker.java
@@ -1,18 +1,45 @@
 import TurtleGraphics.*;
+import TerminalIO.*;
+import java.awt.Color;
 
 public class ShapeMaker {
     
     public static void main(String[] args) {
         Shape s;
-        Pen p = new StandardPen();
+        double r,x,y,w,h;
+        KeyboardReader k = new KeyboardReader();
+        int choice;
+        
+        Pen p = new StandardPen(new SketchPadWindow(400,400));
         s = new Circle(10,10,5);
-        System.out.println(s);
-        s.move(0, 0);
-        s.stretchBy(2);
-        System.out.println(s);
-        s = new Rect(0,0,50,25);
-         System.out.println(s);
-         s.draw(p);
+       while(true)
+       {
+           choice=k.readInt("1 For Rect, 2 for Circle: ");
+           if(choice==1 || choice==2) break;
+       }
+       if (choice==1)
+       {
+           w=k.readInt("Enter width of Rect: ");
+           h=k.readInt("Enter height of Rect: ");
+           s=new Rect(0,0,w,h);
+       }
+       else
+       {
+           r=k.readInt("Enter radius of Circle: ");
+           s=new Circle(0,0,r);
+       }
+       while(true)
+       {
+           s.draw(p);
+           x=k.readDouble("New x?: ");
+           y=k.readDouble("New y?: ");
+           p.setColor(Color.white);
+           s.draw(p);
+           s.move(x, y);
+           p.setColor(Color.blue);
+           s.draw(p);
+       }
+       
     }
     
 }
