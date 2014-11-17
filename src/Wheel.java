5793c573ac9cb82dab6a7174944b3069efcc86e1
conn6070 <conn6070@ISS147D8-03.scdsb.pri>
2014-11-17 09:19:45 -0500

    lesson 9

--- src/Wheel.java
+++ src/Wheel.java
@@ -1,3 +1,37 @@
+
+import TurtleGraphics.Pen;
+
 public class Wheel extends Circle{
-    
+    private int spokes;
+    public Wheel()
+    {
+        super();
+        spokes = 0;
+    }
+    public Wheel (double xLoc, double yLoc,double r, int s) 
+    {
+        super (xLoc, yLoc, r);
+        spokes = s;
+    }
+    public void draw(Pen p)
+    {
+        super.draw(p);
+        for (int i =1; i <= spokes; i++)
+        {
+            p.up();
+            p.move (xPos, yPos);
+            p.setDirection (i * 360.0  / spokes);
+            p.down();
+            p.move (radius);
+        }
+    }
+    public void setSpokes (int s) 
+    {
+        spokes = s;
+    }
+    public string toString()
+    {
+        String str = "Wheel\n" + "radius: " + radius + "\n" + "Spokes: " + spokes + "\n" + "(x,y) Position: (" + xPos + ","+yPos + ")\n" + "Area: " + area();
+        return str();
+    }
 }
